57ba03b8f69dd67e43386a810f0f5157910364f6
Layse Garcia <63252079+layse-garcia@users.noreply.github.com>
2022-06-03 00:56:36 +0100

    Update Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,46 +1,141 @@
-import React,{useState,useEffect} from 'react';
-import { Text, Button, View, Image, LogBox } from 'react-native';
-import styles from "../styles/main";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StatusBar } from 'expo-status-bar';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 
-const Home =(props) => {
+// https://www.kindacode.com/article/how-to-set-a-gradient-background-in-react-native/
+// expo install expo-linear-gradient
+import { LinearGradient } from 'expo-linear-gradient';
 
-  //to set the value on Text
+// https://www.geeksforgeeks.org/how-to-add-searchbar-in-react-native/
+// npm install --global expo-cli
 
-  const [getValueName,setGetName]= useState("");
-  const[getValueCity,setGetCity]= useState("");
+//npm i react-native-picker-select
+import RNPickerSelect from "react-native-picker-select";
 
-  const getData = async ()=> {
-    //function to get the value from AsyncStorage 
+import { Posts } from './Posts';
 
-    AsyncStorage.getItem("name").then(
-      (value) => 
-      //AsyncStorage returns a promise,so adding a callback to get the value 
-      setGetName(value)
-      //Seting the value in text
-    );
-    AsyncStorage.getItem("city").then(
-      (value) => 
-      //AsyncStorage returns a promise,so adding a callback to get the value 
-      setGetCity(value)
-      //Seting the value in text
-    );
-  };
-  useEffect(()=> {
-    getData();
-  }, []);
-        return(
-            <View style= {styles.container}>
+import Brand from "../assets/img/logo.png";
 
-          <Text style={styles.header}>
-            Hi {getValueName} from {getValueCity}
-          </Text>
-          <View style= {styles.container}>
 
-<Image style={styles.img_logo} source={require('../assets/logo.png')}/>
-            </View>
-            </View>
-        );
-        };
+// https://reactnavigation.org/docs/tab-based-navigation/
+//https://reactnavigation.org/docs/drawer-based-navigation/
 
-export default Home;
+
+export const Home = () => {
+  return (
+    <View>
+      <View style={[styles.container, {
+      // Try setting `flexDirection` to `"row"`.
+      flexDirection: "column"
+    }]}>
+        <LinearGradient 
+          colors={['rgba(69,203,178,0.2)', 'rgba(69,203,178,1)']} 
+          start={{
+            x: 0,
+            y: 350
+          }}
+          end={{
+            x: 1,
+            y: 1
+          }} 
+          style={styles.box}>
+          
+          <Image source={Brand} style={styles.logo} />
+          <Text style={styles.user_name}>Utilizador Name</Text>
+          <Text style={styles.description}>A partilhar os melhores projetos em 3D</Text>
+
+          <TextInput
+            onChangeText={() => onChange()}
+            value={'Procurar'}
+            style={styles.search}
+          />
+          <TextInput
+            onChangeText={() => onChange()}
+            value={'Categoria...'}
+            style={styles.select_input}
+          />
+        </LinearGradient>
+        <Text style={styles.title_comunity}>Comunidade</Text>
+        <Posts/>
+      </View>
+        
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  },
+  container:{
+    textAlign: 'center',
+    marginBottom: 20,
+    width: '100%',
+  },
+  logo: {
+    width: 50,
+    textAlign: 'center',
+    position: 'relative',
+    marginLeft: '10%',
+    marginRight: '10%',
+  },
+  search: {
+    position: 'relative',
+    borderWidth: 1,
+    borderRadius: 21,
+    fontSize: 14,
+    width: 313,
+    height: 43,
+    backgroundColor: 'white',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    
+  },
+  select_input: {
+    position: 'relative',
+    borderWidth: 1,
+    borderRadius: 21,
+    fontSize: 14,
+    width: 313,
+    height: 43,
+    backgroundColor: 'white',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  user_name:  {
+    position: 'relative',
+    textAlign: 'center',
+    width: 88,
+    height: 22,
+    font: 'Montserrat',
+    fontWeight: 700,
+    fontSize: 18,
+    lineHeight: 22,
+    color: '#2C2C2C',
+    marginTop: 50,
+  },
+  description:{
+    position: 'relative',
+    width: 315,
+    height: 25,
+    font: 'Montserrat',
+    fontWeight: 300,
+    fontSize: 16,
+    lineHeight: 25,
+    color: '#2C2C2C',
+    marginTop: 60,
+  },
+  title_comunity:{
+    marginTop: 10,
+    font: 'Montserrat',
+    fontWeight: 700,
+    fontSize: 18,
+    lineHeight: 22,
+    color: '#2C2C2C',
+  }
+});
