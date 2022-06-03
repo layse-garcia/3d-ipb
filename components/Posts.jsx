aba8b8d04fe5ac693d675196096e19a0e51426b6
Layse Garcia <63252079+layse-garcia@users.noreply.github.com>
2022-06-03 01:00:06 +0100

    Update Posts.jsx

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,61 +1,92 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import {Text, Button, View, Image, FlatList, ActivityIndicator } from 'react-native';
-
-import styles from "../styles/main";
-
-const Posts = (props) => {
-    //Hook useState: Similar to 'this.state' in class components
-    // https://reactjs.org/docs/hooks-state.html
-
-
-      const [isLoading,setLoading]= useState(true);
-      const [data,setData]=useState([]);
-      const uri='https://jsonplaceholder.typicode.com/posts/';
-
-    //Hook useEffect : Similar to componentDidMount and componentDidUpdate
-    // https://reactjs.org/docs/hooks-effect.html
-
-
-    useEffect(()=>{
-        fetch(uri)
-        .then((response) => response.json())
-        .then((json) => setData(json))
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
-    },[]);
-
-      const ListPosts = ({ post }) =>{
-        return (
-              <View style= {styles.postListItem}>
-                  <Text style={styles.postTitle}>Post #{post.id} - {post.title.slice(0,30)}...</Text>
-                  <Text style={styles.postBody}> {post.title.slice(0,60)}...</Text>
-                  <View style={styles.postSeparator}></View>
-                <View style={styles.postButton}>
-          <Button title = 'See More' 
-          onPress={()=> props.navigation.navigate('Post',{post: post})}/>
-                </View>
-              </View>
-        );
-      };
-
-      return(
-        <View style={styles.container}>
-          {isLoading ?
-          <ActivityIndicator />
-          :
-          (
-            <FlatList
-            style={styles.flatlist}
-            data={data}
-            keyExtractor= {item => item.id.toString()}
-            renderItem={({item})=> <ListPosts post ={item} />}
-            />
-          )
-        }
-        </View>
-    );
-};
-
-export default Posts;
+import React, { Component } from 'react';
+ 
+import { AppRegistry, StyleSheet, FlatList, Text, View, Alert, Platform } from 'react-native';
+ 
+export class Posts extends Component {
+ 
+ constructor(props)
+ {
+   super(props);
+ 
+   this.state = { GridViewItems: [
+     {key: 'One'},
+     {key: 'Two'},
+     {key: 'Three'},
+     {key: 'Four'},
+     {key: 'Five'},
+     {key: 'Six'},
+     {key: 'Seven'},
+     {key: 'Eight'},
+     {key: 'Nine'},
+     {key: 'Ten'},
+     {key: 'Eleven'},
+     {key: 'Twelve'},
+   ]}
+ }
+ 
+ GetGridViewItem (item) {
+  
+ Alert.alert(item);
+ 
+ }
+ 
+ 
+ render() {
+   return (
+ 
+<View style={styles.MainContainer}>
+ 
+      <FlatList
+      
+         data={ this.state.GridViewItems }
+ 
+         renderItem={({item}) =><View style={styles.GridViewBlockStyle}>
+ 
+            <Text style={styles.GridViewInsideTextItemStyle} onPress={this.GetGridViewItem.bind(this, item.key)} > {item.key} </Text>
+            
+            </View>}
+ 
+         numColumns={3}
+ 
+        />
+   
+   
+</View>
+           
+   );
+ }
+}
+ 
+const styles = StyleSheet.create({
+ 
+MainContainer :{
+ 
+justifyContent: 'center',
+flex:1,
+margin: 10,
+  alignItems: 'center',
+paddingTop: (Platform.OS) === 'ios' ? 20 : 0
+ 
+},
+ 
+GridViewBlockStyle: {
+ 
+  
+  height: 100,
+  width: 100,
+  margin: 5,
+  backgroundColor: '#00BCD4',
+ 
+}
+,
+ 
+GridViewInsideTextItemStyle: {
+ 
+   color: '#fff',
+   padding: 10,
+   fontSize: 18,
+ },
+ 
+});
+ 
+AppRegistry.registerComponent('Posts', () => Posts);
